76974c060bfb98be3570786f308610803ce2f2ae
saniya-ranjan <saniyaranjan355@gmail.com>
2026-05-25 07:06:54 +0530

    Deployment setup

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3##u9n1z(!ya!f368dfa8yj(%y1snuhkpjo)$7wf9wd@md2^_-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
